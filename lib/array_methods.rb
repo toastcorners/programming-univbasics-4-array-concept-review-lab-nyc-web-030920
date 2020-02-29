4448d15eb8f44b0d7c0a8aa064c4a301d82d3a4d
Melissa Wise <wise.melissa19@gmail.com>
2020-02-29 07:58:08 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ array.index value_to_find
 end
 
 def find_max_value(array)
-  # Add your solution here
+  sorted_max = array.sort
+  return sorted_max.last
 end
 
 def find_min_value(array)
-  # Add your solution here
+  sorted = array.sort
+  return sorted.first
 end
